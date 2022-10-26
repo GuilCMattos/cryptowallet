ac87b9ab740e379cde547221db4cb7d415ea6e55
Guilherme <mttsguil@gmail.com>
2022-10-26 15:34:57 -0300

    refec: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Coin.create!(
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas..")
+spinner.auto_spin
+
+  coins = [  {
     description: "Bitcoin",
     acronym: "BTC",
     url_image: "https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"
-)
+    },
 
-Coin.create!(
+    {
     description: "Ethereum",
     acronym: "ETH",
     url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
-)
+    }]
+
+    coins.each do |coin| 
+        Coin.find_or_create_by!(coin)
+    end
+
+    spinner.success("Cadastrado com sucesso!")
