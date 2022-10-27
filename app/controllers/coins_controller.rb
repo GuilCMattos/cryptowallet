463e9069bf31b868e55d29e35d405ecb6097f26e
Guilherme <mttsguil@gmail.com>
2022-10-27 16:23:09 -0300

    add: padronize select

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,6 +1,7 @@
 class CoinsController < ApplicationController
   layout "adm"
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :edit, :update, :create]
 
   # GET /coins or /coins.json
   def index
@@ -62,6 +63,10 @@ class CoinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
+    end
+
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
     end
 
     # Only allow a list of trusted parameters through.
